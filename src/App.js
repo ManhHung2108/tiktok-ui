d217aa0441f9b84b18e4763cf99ca9157284356f
ManhHung <manhhung210801@gmail.com>
2023-05-18 17:33:01 +0700

    Cài đặt và cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Button from '~/component/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button />
+        </div>
+    );
 }
 
 export default App;
